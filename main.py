d2897e8a2f751120f23a8ae46e129d8c3e91ea6b
Mael <legrand.mael2002@gmail.com>
2020-12-01 15:06:27 +0100

    Grayscale Test

---
+++ main.py
@@ -0,0 +1,12 @@
+from art import *
+import cv2
+
+def grayscale():
+    image = cv2.imread('data/imgs/img1.png')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Original image', image)
+    cv2.imshow('Gray image', gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+grayscale()
